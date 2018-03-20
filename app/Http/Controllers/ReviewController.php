893905aaef15b79281099ac5d6839fe983045a1b
truptigaonkar <trups11.trupti@gmail.com>
2018-03-20 14:30:27 +0100

    Created ReviewResource to list review as per the product

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Model\Review;
 use Illuminate\Http\Request;
+use App\Http\Resources\ReviewResource;
+use App\Model\Product;
+
 
 class ReviewController extends Controller
 {
@@ -12,9 +15,11 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Product $product)
     {
-        //
+        //return Review::all();
+        //return $product->reviews;
+        return ReviewResource::collection($product->reviews);
     }
 
     /**
